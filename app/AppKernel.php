95d2201369e7166ee1cd7fe21c5d5e775f897ec1
Roman Wollenburg <roman@slash-works.de>
2015-07-15 10:07:12 +0200

    timezone-default value added, missing translations added, tooltips for remoteapplist added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,6 +9,41 @@
     class AppKernel extends Kernel
     {
 
+
+        public function __construct($environment, $debug)
+        {
+
+            if(!ini_get('date.timezone') || !date_default_timezone_get()){
+                $timezone = 'Europe/Berlin';
+                if (is_link('/etc/localtime')) {
+                    // Mac OS X (and older Linuxes)
+                    // /etc/localtime is a symlink to the
+                    // timezone in /usr/share/zoneinfo.
+                    $filename = readlink('/etc/localtime');
+                    if (strpos($filename, '/usr/share/zoneinfo/') === 0) {
+                        $timezone = substr($filename, 20);
+                    }
+                } elseif (file_exists('/etc/timezone')) {
+                    // Ubuntu / Debian.
+                    $data = file_get_contents('/etc/timezone');
+                    if ($data) {
+                        $timezone = $data;
+                    }
+                } elseif (file_exists('/etc/sysconfig/clock')) {
+                    // RHEL / CentOS
+                    $data = parse_ini_file('/etc/sysconfig/clock');
+                    if (!empty($data['ZONE'])) {
+                        $timezone = $data['ZONE'];
+                    }
+                }
+                date_default_timezone_set($timezone);
+            }
+
+            parent::__construct($environment, $debug);
+        }
+
+
+
         /**
          * @return array
          */
